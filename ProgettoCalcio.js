d032f26cc57602b5714c7853d0e32f988df701de
Giacche <giacchetti125@gmail.com>
2019-09-23 15:41:21 +0200

    Inizializzo la libreria express e il valore della porta su cui ascoltera' il mio progetto

--- ProgettoCalcio.js
+++ ProgettoCalcio.js
@@ -2,3 +2,9 @@
 const express = require ('express');
 const request = require ('request');
 
+/* Inizializzazione delle librerie */
+const SerieA = require ('./SerieA.json');
+const app = express();
+
+/* Inizializzazione della Porta */
+const PORTA = process.env.port || 4600;
