557c10c78630fe2297d874527bd6cb752da58b1e
Giacche <giacchetti125@gmail.com>
2019-09-23 15:51:22 +0200

    Modifiche

--- ProgettoCalcio.js
+++ ProgettoCalcio.js
@@ -15,7 +15,7 @@ const PORTA = process.env.port || 4600;
 
 
 
-/* Funzione per far sapere all'utente la porta utilizzata e per far capire che tutto sta funzionando*/
+/* Funzione per far sapere all'utente la porta utilizzata e per far capire che tutto sta funzionando */
 const listener = app.listen(PORT, () => {
 	console.log('La tua applicazione sta ascoltando sulla porta' + listener.address().port);
  });
