1cfad4472e37e5935fc56198e72ce39aea7a0184
Giacche <giacchetti125@gmail.com>
2019-09-23 15:48:06 +0200

    Creazione funzione per far sapere all'utente la porta utilizzata

--- ProgettoCalcio.js
+++ ProgettoCalcio.js
@@ -8,3 +8,14 @@ const app = express();
 
 /* Inizializzazione della Porta */
 const PORTA = process.env.port || 4600;
+
+
+
+
+
+
+
+/* Funzione per far sapere all'utente la porta utilizzata e per far capire che tutto sta funzionando*/
+const listener = app.listen(PORT, () => {
+	console.log('La tua applicazione sta ascoltando sulla porta' + listener.address().port);
+ });
