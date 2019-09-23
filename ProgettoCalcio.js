eb8f767148ac60cc7b7dc689a970d769f34115ff
Giacche <giacchetti125@gmail.com>
2019-09-23 15:56:28 +0200

    Modifiche per sistemare degli errori che impediscono al programma di partire

--- ProgettoCalcio.js
+++ ProgettoCalcio.js
@@ -10,13 +10,13 @@ const app = express();
 const PORTA = process.env.port || 4600;
 
 /* Funzione per visualizzare sul servizio Web tutte le informazioni messe a disposizione dall'API utilizzata */
-c.get('/' , (req, res) => {
+app.get('/' , (req, res) => {
 
 	res.send(SerieA);
 	res.end();
 	});
 
 /* Funzione per far sapere all'utente la porta utilizzata e per far capire che tutto sta funzionando */
-const listener = app.listen(PORT, () => {
+const listener = app.listen(PORTA, () => {
 	console.log('La tua applicazione sta ascoltando sulla porta' + listener.address().port);
  });
