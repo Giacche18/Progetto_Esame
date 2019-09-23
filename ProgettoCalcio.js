da24c1f9940f71715a2ce7947dbef53118605bbd
Giacche <giacchetti125@gmail.com>
2019-09-23 15:53:50 +0200

    Creazione funzione per visualizzare le informazioni dell'API utilizzata

--- ProgettoCalcio.js
+++ ProgettoCalcio.js
@@ -9,11 +9,12 @@ const app = express();
 /* Inizializzazione della Porta */
 const PORTA = process.env.port || 4600;
 
+/* Funzione per visualizzare sul servizio Web tutte le informazioni messe a disposizione dall'API utilizzata */
+c.get('/' , (req, res) => {
 
-
-
-
-
+	res.send(SerieA);
+	res.end();
+	});
 
 /* Funzione per far sapere all'utente la porta utilizzata e per far capire che tutto sta funzionando */
 const listener = app.listen(PORT, () => {
